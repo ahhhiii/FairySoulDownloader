de014bcc56528b3c16dc573b03387d6eec849452
alexh <alex.hammonnd003@gmail.com>
2022-05-04 03:44:34 +0100

    Started main method

--- src/main/java/net/bruhitsalex/fairysouldownloader/Main.java
+++ src/main/java/net/bruhitsalex/fairysouldownloader/Main.java
@@ -2,14 +2,25 @@ package net.bruhitsalex.fairysouldownloader;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
         Document doc = Jsoup.connect("https://hypixel-skyblock.fandom.com/wiki/Fairy_Souls").get();
-        System.out.println(doc.body().toString());
+
+        List<Element> elements = doc.getAllElements()
+                .stream()
+                .filter(element -> element.id().startsWith("mw-customcollapsible-"))
+                .collect(Collectors.toList());
+
+        for (Element element : elements) {
+            System.out.println(element.id());
+        }
     }
 
 }
